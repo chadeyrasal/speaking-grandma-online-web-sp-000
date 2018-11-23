3f1b762d3f5e879e972c0431092d4e6400ca332c
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 14:31:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,11 +13,11 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase
+  if phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   elsif phrase == phrase.capitalize
     "HUH?! SPEAK UP, SONNY!"
+  else
+    "I LOVE YOU TOO PUMPKIN!"
   end
 end
