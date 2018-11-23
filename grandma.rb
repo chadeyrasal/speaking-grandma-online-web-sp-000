d1d4483d3d2ff1b2ca14f48395fad8f4eae95756
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 14:59:13 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == !(phrase.upcase) && phrase != "I LOVE YOU GRANDMA!"
+  elsif phrase == phrase.capitalize && phrase != "I LOVE YOU GRANDMA!"
     "HUH?! SPEAK UP, SONNY!"
   end
 end
