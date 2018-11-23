ca4022bb1e2db50eac3ae826f3bbc63d64af4f86
Anne-Laure Chadeyras <annelaure.chadeyras@gmail.com>
2018-11-23 14:56:43 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
   elsif phrase = "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   else
-    "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
